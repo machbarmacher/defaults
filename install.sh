0ccb46f043ceb3eea540772861d5dd3a74ca939f
Axel Rutz <axel.rutz@clever-systems.net>
2018-02-12 21:30:02 +0100

    Worked around https://www.drupal.org/project/config_rewrite/issues/2944231

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 composer require machbarmacher/defaults --no-progress --prefer-dist
 [-d docroot ] && cd docroot || [-d web ] && cd web
-drush en -y shield backup_migrate drush_language
+drush en -y defaults_modules && drush en -y defaults_config
 
